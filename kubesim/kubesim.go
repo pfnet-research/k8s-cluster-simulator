1991a382dcd0ef665adeb14adbd4ee305b411772
Hidehito Yabuuchi <hdht.ybuc@gmail.com>
2019-02-12 17:36:41 +0900

    Avoid dynamic memory allocation in {kubesim,scheduler/extender}.go

--- kubesim/kubesim.go
+++ kubesim/kubesim.go
@@ -211,7 +211,7 @@ func configLog(logLevel string) error {
 
 // List implements "k8s.io/pkg/scheduler/algorithm".NodeLister
 func (k *KubeSim) List() ([]*v1.Node, error) {
-	nodes := []*v1.Node{}
+	nodes := make([]*v1.Node, 0, len(k.nodes))
 	for _, node := range k.nodes {
 		nodes = append(nodes, node.ToV1())
 	}
