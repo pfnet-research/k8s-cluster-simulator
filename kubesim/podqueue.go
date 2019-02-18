5f0f7b6072b7b4c62fd95448ab41324da93f0f51
Hidehito Yabuuchi <hdht.ybuc@gmail.com>
2019-02-18 16:18:41 +0900

    Remove locks in podqueue.go as the submission and scheduling are synch'ed

--- kubesim/podqueue.go
+++ kubesim/podqueue.go
@@ -2,23 +2,17 @@ package kubesim
 
 import (
 	"errors"
-	"sync"
 
 	v1 "k8s.io/api/core/v1"
 )
 
 // podQueue stores pods in a queue.
-// It wraps []*v1.Pod for thread-safetiness.
 type podQueue struct {
 	queue []*v1.Pod
-	lock  sync.Mutex
 }
 
 // append pushes a pod to this podQueue.
 func (q *podQueue) append(pod *v1.Pod) {
-	q.lock.Lock()
-	defer q.lock.Unlock()
-
 	q.queue = append(q.queue, pod)
 }
 
@@ -28,9 +22,6 @@ var errEmptyPodQueue = errors.New("No pod queued")
 // pop pops a pod from this podQueue.
 // If this podQueue is empty, errEmptyPodQueue will be returned.
 func (q *podQueue) pop() (*v1.Pod, error) {
-	q.lock.Lock()
-	defer q.lock.Unlock()
-
 	if len(q.queue) == 0 {
 		return nil, errEmptyPodQueue
 	}
