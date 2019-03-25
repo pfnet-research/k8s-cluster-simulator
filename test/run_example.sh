b7b1de60f4f1ed613a2e3af669b620e4ae579698
Hidehito Yabuuchi <hdht.ybuc@gmail.com>
2019-03-25 16:30:59 +0900

    Add comment to test/run_example.sh

--- test/run_example.sh
+++ test/run_example.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# Tests that the example app finishes successfully without being stucked.
+
 cd $(git rev-parse --show-toplevel)
 
 function err() {
-    echo -e "\033[031m" $1 "\033[0m"
+    echo -e "\033[031m$1\033[0m"
     exit $2
 }
 
@@ -13,7 +15,7 @@ timeout $sec $cmd >& /dev/null
 
 status=$?
 if [ $status -eq 124 ]; then
-    err "$cmd timeouted: took over $sec s" $status
+    err "'$cmd' timeouted: took over $sec s" $status
 elif [ $status -ne 0 ]; then
-    err "$cmd failed" $status
+    err "'$cmd' failed" $status
 fi
