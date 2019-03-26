910b8973f4c7578c10a57f5dc82bf14505b95b04
Hidehito Yabuuchi <hdht.ybuc@gmail.com>
2019-03-26 14:12:58 +0900

    Add comments to kubesim.go

--- pkg/kubesim.go
+++ pkg/kubesim.go
@@ -40,7 +40,7 @@ import (
 	"github.com/ordovicia/k8s-cluster-simulator/pkg/util"
 )
 
-// KubeSim represents a kubernetes cluster simulator.
+// KubeSim represents a simulated kubernetes cluster.
 type KubeSim struct {
 	tick  time.Duration
 	clock clock.Clock
@@ -57,7 +57,11 @@ type KubeSim struct {
 }
 
 // NewKubeSim creates a new KubeSim with the given config, queue, and scheduler.
-func NewKubeSim(conf *config.Config, queue queue.PodQueue, sched scheduler.Scheduler) (*KubeSim, error) {
+// Returns error if the configuration failed.
+func NewKubeSim(
+	conf *config.Config, queue queue.PodQueue, sched scheduler.Scheduler,
+) (*KubeSim, error) {
+
 	log.G(context.TODO()).Debugf("Config: %+v", *conf)
 
 	if err := configLog(conf.LogLevel); err != nil {
@@ -102,7 +106,11 @@ func NewKubeSim(conf *config.Config, queue queue.PodQueue, sched scheduler.Sched
 
 // NewKubeSimFromConfigPath creates a new KubeSim with config from confPath (excluding file path),
 // queue, and scheduler.
-func NewKubeSimFromConfigPath(confPath string, queue queue.PodQueue, sched scheduler.Scheduler) (*KubeSim, error) {
+// Returns error if the configuration failed.
+func NewKubeSimFromConfigPath(
+	confPath string, queue queue.PodQueue, sched scheduler.Scheduler,
+) (*KubeSim, error) {
+
 	conf, err := readConfig(confPath)
 	if err != nil {
 		return nil, errors.Errorf("Error reading config: %s", err.Error())
@@ -111,13 +119,14 @@ func NewKubeSimFromConfigPath(confPath string, queue queue.PodQueue, sched sched
 	return NewKubeSim(conf, queue, sched)
 }
 
-// AddSubmitter adds a new submitter plugin to this KubeSim.
+// AddSubmitter adds the new submitter to this KubeSim.
 func (k *KubeSim) AddSubmitter(name string, submitter submitter.Submitter) {
 	k.submitters[name] = submitter
 }
 
-// Run executes the main loop, which invokes scheduler plugins and binds pods to the selected nodes.
-// This method blocks until ctx is done.
+// Run executes the main loop, which invokes submitters and the scheduler, and binds pods to the
+// selected nodes.
+// This method blocks until ctx is done or this KubeSim finishes processing all pods.
 func (k *KubeSim) Run(ctx context.Context) error {
 	preMetricsClock := k.clock
 	met, err := metrics.BuildMetrics(k.clock, k.nodes, k.pendingPods)
@@ -130,7 +139,7 @@ func (k *KubeSim) Run(ctx context.Context) error {
 	for {
 		if k.toTerminate(submitterAddedEver) {
 			log.L.Debug("Terminate KubeSim")
-			return nil
+			break
 		}
 		submitterAddedEver = submitterAddedEver || len(k.submitters) > 0
 
@@ -140,14 +149,15 @@ func (k *KubeSim) Run(ctx context.Context) error {
 		default:
 			log.L.Debugf("Clock %s", k.clock.ToRFC3339())
 
-			if err = k.submit(met); err != nil {
+			if k.submit(met) != nil {
 				return err
 			}
 
-			if err = k.schedule(); err != nil {
+			if k.schedule() != nil {
 				return err
 			}
 
+			// Rebuild metrics every tick for submitters to use.
 			met, err = metrics.BuildMetrics(k.clock, k.nodes, k.pendingPods)
 			if err != nil {
 				return err
@@ -165,9 +175,12 @@ func (k *KubeSim) Run(ctx context.Context) error {
 			k.clock = k.clock.Add(k.tick)
 		}
 	}
+
+	return nil
 }
 
-// List implements "k8s.io/pkg/scheduler/algorithm".NodeLister
+// List implements "k8s.io/pkg/scheduler/algorithm".NodeLister interface.
+// Never returns an error.
 func (k *KubeSim) List() ([]*v1.Node, error) {
 	nodes := make([]*v1.Node, 0, len(k.nodes))
 	for _, node := range k.nodes {
@@ -199,7 +212,7 @@ func readConfig(path string) (*config.Config, error) {
 }
 
 func configLog(logLevel string) error {
-	level, err := log.ParseLevel(logLevel) // if logLevel == "", level <- info
+	level, err := log.ParseLevel(logLevel) // if logLevel == "" then level <- info
 	if err != nil {
 		return strongerrors.InvalidArgument(
 			errors.Errorf("Log level %q not supported: %s", level, err.Error()))
@@ -268,15 +281,18 @@ func buildMetricsWriters(conf *config.Config) ([]metrics.Writer, error) {
 	return writers, nil
 }
 
+// toTerminate determins whether the main loop of this KubeSim can be terminated,
+// because all submitters are terminated, no pods are running on the cluster, and there are no
+// pending pods in the queue.
 func (k *KubeSim) toTerminate(submitterAddedEver bool) bool {
-	if _, err := k.pendingPods.Front(); err == queue.ErrEmptyQueue {
-		for _, node := range k.nodes {
+	if _, err := k.pendingPods.Front(); err == queue.ErrEmptyQueue { // queue is empty
+		for _, node := range k.nodes { // cluster is empty
 			if node.PodsNum(k.clock) > 0 {
 				return false
 			}
 		}
 
-		if submitterAddedEver && len(k.submitters) == 0 {
+		if submitterAddedEver && len(k.submitters) == 0 { // all submitters are terminated
 			return true
 		}
 	}
@@ -313,9 +329,7 @@ func (k *KubeSim) submit(metrics metrics.Metrics) error {
 				log.L.Debugf("Submitter %s: Delete %s",
 					name, util.PodKeyFromNames(del.PodNamespace, del.PodName))
 
-				deletedFromQueue := k.pendingPods.Delete(del.PodNamespace, del.PodName)
-
-				if !deletedFromQueue {
+				if delFromQ := k.pendingPods.Delete(del.PodNamespace, del.PodName); !delFromQ {
 					k.deletePodFromNode(del.PodNamespace, del.PodName)
 				}
 			} else if up, ok := e.(*submitter.UpdateEvent); ok {
@@ -344,16 +358,19 @@ func (k *KubeSim) submit(metrics metrics.Metrics) error {
 }
 
 func (k *KubeSim) schedule() error {
+	// Build up-to-date NodeInfo.
 	nodeInfoMap := make(map[string]*nodeinfo.NodeInfo, len(k.nodes))
 	for name, node := range k.nodes {
 		nodeInfoMap[name] = node.ToNodeInfo(k.clock)
 	}
 
+	// The scheduler makes scheduling decision.
 	events, err := k.scheduler.Schedule(k.clock, k.pendingPods, k, nodeInfoMap)
 	if err != nil {
 		return err
 	}
 
+	// Do the actual scheduling process for each event.
 	for _, e := range events {
 		if bind, ok := e.(*scheduler.BindEvent); ok {
 			nodeName := bind.ScheduleResult.SuggestedHost
